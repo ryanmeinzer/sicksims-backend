e7923dc52d998be8b0851d7a94ac0a5db0c2e5f7
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-09-05 11:05:45 -0400

    implimented basic backend input field profanity and spam filter

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,5 +1,7 @@
 class Superhero < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: true
+    validates :name, exclusion: { in: %w(fuck shit damn horny bitch cunt ass gay anal balls blowjob clit cock dick dildo fag god homo jizz cum nigger nigga penis vagina pussy queer slut tit twat whore) }
+    validates :name, format: { with: /\A[a-z[ ]['][-][(][)][!][?][,]]+\Z/i }
     validates :score, presence: true
 end
