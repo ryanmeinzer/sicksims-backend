b1a2f1ff929cc08d6be6a29500b9ca3ffdd971cc
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-09-05 12:43:09 -0400

    re-added input validations now that mobile input issue is resolved

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,8 +1,8 @@
 class Superhero < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: true
-    # validates :name, exclusion: { in: %w(fuck fucker shit damn horny bitch cunt ass gay anal balls blowjob clit cock dick dildo fag god homo jizz cum nigger nigga penis vagina pussy queer slut tit twat whore) }
-    # validates :name, format: { with: /\A[a-z[ ]['][-][(][)][!][?][,]]+\Z/i }
-    # validates :name, length: { maximum: 15 }
+    validates :name, exclusion: { in: %w(fuck fucker shit damn horny bitch cunt ass gay anal balls blowjob clit cock dick dildo fag god homo jizz cum nigger nigga penis vagina pussy queer slut tit twat whore) }
+    validates :name, format: { with: /\A[a-z[ ]['][-][(][)][!][?][,]]+\Z/i }
+    validates :name, length: { maximum: 15 }
     validates :score, presence: true
 end
