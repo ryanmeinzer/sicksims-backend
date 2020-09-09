43151a88f0ccc8bb984e21502c134db6b9c43f81
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-09-09 14:44:56 -0400

    allowed * in input and extended max characters to 20

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -2,7 +2,7 @@ class Superhero < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: true
     validates :name, exclusion: { in: %w(fuck fucker shit damn horny bitch cunt ass gay anal balls blowjob clit cock dick dildo fag god homo jizz cum nigger nigga penis vagina pussy queer slut tit twat whore) }
-    validates :name, format: { with: /\A[a-z[ ]['][-][(][)][!][?][,]]+\Z/i }
-    validates :name, length: { maximum: 15 }
+    validates :name, format: { with: /\A[a-z[ ][*]['][-][(][)][!][?][,]]+\Z/i }
+    validates :name, length: { maximum: 20 }
     validates :score, presence: true
 end
